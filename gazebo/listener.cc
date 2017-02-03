d8434c27c0647210db1d2c29cff4f036830978b2
cahartsell <charles.a.hartsell@vanderbilt.edu>
2017-02-03 17:13:55 -0600

    Added initial TCP server functionality to listener.

--- gazebo/listener.cc
+++ gazebo/listener.cc
@@ -21,6 +21,21 @@
 #include <ignition/math/Pose3.hh>
 
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define PORT "18463"
+#define BACKLOG 10
 
 /////////////////////////////////////////////////
 // Function is called everytime a message is received.
@@ -29,7 +44,26 @@ void cb(ConstLaserScanStampedPtr &_msg)
   gazebo::msgs::LaserScan lScan;
   lScan = _msg->scan();
   // Dump the message contents to stdout.
-  std::cout << lScan.ranges(0) << "\n";
+  std::cout << lScan.ranges(0) << std::endl;
+}
+
+/////////////////////////////////////////////////////
+// Networking helper functions
+void sigchld_handler(int s)
+{
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+    errno = saved_errno;
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 /////////////////////////////////////////////////
@@ -42,34 +76,98 @@ int main(int _argc, char **_argv)
   gazebo::transport::NodePtr node(new gazebo::transport::Node());
   node->Init();
 
-  // Construct publisher
+  // Construct publisher and wait for listener
   gazebo::transport::PublisherPtr velCmdPub = node->Advertise<gazebo::msgs::Pose>("~/create/vel_cmd");
   velCmdPub->WaitForConnection();
   
-  // Listen to Gazebo topics
-  gazebo::transport::SubscriberPtr wall_sensor_sub = node->Subscribe("~/create/base/wall_sensor/scan", cb);
-  gazebo::transport::SubscriberPtr left_sensor_sub = node->Subscribe("~/create/base/left_cliff_sensor/scan", cb);
-  gazebo::transport::SubscriberPtr leftfront_sensor_sub = node->Subscribe("~/create/base/leftfront_cliff_sensor/scan", cb);
-  gazebo::transport::SubscriberPtr right_sensor_sub = node->Subscribe("~/create/base/right_cliff_sensor/scan", cb);
-  gazebo::transport::SubscriberPtr rightfront_sensor_sub = node->Subscribe("~/create/base/rightfront_cliff_sensor/scan", cb);
+  // Subscribe to Gazebo topics
+  gazebo::transport::SubscriberPtr wall_sensor_sub, left_sensor_sub, leftfront_sensor_sub, right_sensor_sub, rightfront_sensor_sub;
+  std::string base_path = "~/create/base/";
+  wall_sensor_sub       = node->Subscribe( base_path + "wall_sensor/scan", cb);
+  left_sensor_sub       = node->Subscribe( base_path + "left_cliff_sensor/scan", cb);
+  leftfront_sensor_sub  = node->Subscribe( base_path + "leftfront_cliff_sensor/scan", cb);
+  right_sensor_sub      = node->Subscribe( base_path + "right_cliff_sensor/scan", cb);
+  rightfront_sensor_sub = node->Subscribe( base_path + "rightfront_cliff_sensor/scan", cb);
 
-  /*
-  gazebo::math::Vector3d wheelTorque(0.0, 10.0, 0.0);
-  gazebo::msgs::Vector3d wheelTorqueMsg;
-  gazebo::msgs::set(&wheelTorqueMsg, wheelTorque);
-  gazebo::msgs::Wrench wrenchMsg;
-  wrenchMsg.set_allocated_torque( &wheelTorqueMsg );
-  */
-
-  /*
-  gazebo::msgs::JointCmd cmdMsg;
-  cmdMsg.set_name("create::left_wheel");
-  cmdMsg.set_force(0.1);
-  */
-
-  ignition::math::Pose3<double> pose1(1,0,0,0,0,10);
-  ignition::math::Pose3<double> pose2(0,0,0,0,0,0);
+  // Messages
+  ignition::math::Pose3<double> forward(1,0,0,0,0,0);
+  ignition::math::Pose3<double> stop(0,0,0,0,0,0);
+  ignition::math::Pose3<double> turn(0,0,0,0,0,10);
   gazebo::msgs::Pose msg;
+
+  // Open Communication Socket
+  int sockfd, client_fd;  // listen on sock_fd, new connection on new_fd
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_addr; // connector's address information
+  socklen_t sin_size;
+  struct sigaction sa;
+  int yes=1;
+  char s[INET6_ADDRSTRLEN];
+  int rv;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
+
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+      return 1;
+  }
+
+  // loop through all the results and bind to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+      if ((sockfd = socket(p->ai_family, p->ai_socktype,
+	      p->ai_protocol)) == -1) {
+	  perror("server: socket");
+	  continue;
+      }
+      if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+	      sizeof(int)) == -1) {
+	  perror("setsockopt");
+	  exit(1);
+      }
+      if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+	  close(sockfd);
+	  perror("server: bind");
+	  continue;
+      }
+      break;
+  }
+
+  freeaddrinfo(servinfo); // all done with this structure
+  
+  if (p == NULL)  {
+      fprintf(stderr, "server: failed to bind\n");
+      exit(1);
+  }
+
+  if (listen(sockfd, BACKLOG) == -1) {
+      perror("listen");
+      exit(1);
+  }
+
+  sa.sa_handler = sigchld_handler; // reap all dead processes
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+      perror("sigaction");
+      exit(1);
+  }
+
+  std::cout << "waiting for connection..." << std::endl;
+
+  sin_size = sizeof their_addr;
+  client_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+  if (client_fd == -1) {
+    perror("accept");
+    continue;
+  }
+
+  inet_ntop(their_addr.ss_family,
+	    get_in_addr((struct sockaddr *)&their_addr),
+	    s, sizeof s);
+  std::cout << "server: got connection from: " << s << std::endl;
 
   // Busy wait loop...replace with your own code as needed.
   while (true){
@@ -84,3 +182,5 @@ int main(int _argc, char **_argv)
   // Make sure to shut everything down.
   gazebo::client::shutdown();
 }
+
+
